9b90b24d6c06afcccb5e88b611e90a7b6ac88c3e
Jonathan98-dev <59828903+Jonathan98-dev@users.noreply.github.com>
2020-02-11 13:49:29 +0100

    adds slide down animation menu

--- Jonathan/intermeditate/script.js
+++ Jonathan/intermeditate/script.js
@@ -1,22 +1,76 @@
 console.log(items)
 
+$(document).ready(function(){
 
-for(i=0; i< items.length; i++)
-{
-
-	
-
-	$("#container").append("<div id = 'itemBg"+[i]+"' class = 'item'><img src= '"+items[i].image+"' ><p class = 'itemName'>"+items[i].name.toUpperCase()+"</p><p class = 'itemDescription'>"+items[i].desc+"</p><button class ='shoppingbutton'>Add to Cart</button></div>")
-
-
-	if(items[i].available === true)
+	for(i=0; i< items.length; i++)
 	{
-		$("#itemBg"+[i]+"").css("background-color","green")
 
 		
+
+		$("#container").append("<div id = 'itemBg"+[i]+"' class = 'item'><img src= '"+items[i].image+"' ><p class = 'itemName'>"+items[i].name.toUpperCase()+"</p><p class = 'itemDescription'>"+items[i].desc+"</p><button class ='shoppingbutton'>Add to Cart</button></div>")
+
+
+		if(items[i].available === true)
+		{
+			$("#itemBg"+[i]+"").css("background-color","green")
+
+			
+		}
+		else if(items[i].available === false)
+		{
+			$("#itemBg"+[i]+"").css("background-color","indianred")
+		}
 	}
-	else if(items[i].available === false)
+
+
+	//prevent propagation <---
+
+	// $("#cart").on("click",function()
+	// {
+	// 	$("#dropdowncart").toggle(function()
+	// 	{
+	// 		$(this).animate({height:0},200)
+	// 	}, function(){$(this).animate({height:100}, 200)})
+		
+	// })
+
+
+
+
+	 //slide down is closed 
+	 var counter = false
+	$("#cart").on("click", function()
 	{
-		$("#itemBg"+[i]+"").css("background-color","indianred")
-	}
-}
+		console.log("hello")
+		
+		if(counter === false)
+		{
+			$("#dropdowncart").slideDown("slow",function(){})
+
+			console.log(counter)
+			counter = true
+		}
+		else if (counter === true)
+		{
+			$("#dropdowncart").slideUp("slow",function(){})
+
+			counter = false;
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
